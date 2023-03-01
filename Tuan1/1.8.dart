d9476df796bbecb5f8a6a0457c0f298faf2b3b31
MonDz1810 <126653890+MonDz1810@users.noreply.github.com>
2023-03-01 15:45:13 +0700

    Add files via upload

--- Tuan1/1.8.dart
+++ Tuan1/1.8.dart
@@ -1,1 +1,9 @@
+import 'dart:io';
 
+void main() {
+  print("Nhap so dau tien:");
+  int? so1 = int.parse(stdin.readLineSync()!);
+  print("Nhap so thu hai:");
+  int? so2 = int.parse(stdin.readLineSync()!);
+  print("$so2 $so1");
+}
